2a56e72aeaa6d1088eb40dfb75d54ba63c5e609e
Nikola Ivkovic <nikola.ivkovic.contact@gmail.com>
2026-02-25 16:47:57 +0100

    edit text logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,49 @@
-import {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import ImageCard from './components/ImageCard';
-import {Container, Row, Col} from 'react-bootstrap';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word,setWord] = useState('');
-  const [images,setImages] = useState([]);
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
-  
-  const handleSearchSubmit = (e)=>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
 
-
-
-  
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((res)=>res.json())
-      .then((data)=>{
-        setImages([{...data,title: word},...images]);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: word }, ...images]);
       })
-    .catch((error)=>{
-      console.log(error);
-     
-      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-      setWord('');
-
-}
-  const handleDeleteImage = (id)=>{
-    setImages(images.filter((image)=> image.id !== id));
+    setWord("");
+  };
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
   };
 
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
-      <Container className="mt-4"> 
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
         <Row xs={1} md={2} lg={3}>
-          {images.map((image,i)=>(
-            <Col key = {i} className="pb-3">
-            <ImageCard image={image} deleteImage = {handleDeleteImage}/>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
             </Col>
-            ))}
+          ))}
         </Row>
-      </Container> 
+      </Container>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
+  backgroundColor: "lightblue",
 };
 
-const Header = ({title})=>{
-    return (
-        <Navbar style={navbarStyle} data-bs-theme="light">
-          <Container>
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-          </Container>
-        </Navbar>
-    )
-}
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Container>
+        <Logo
+          style={{ maxWidth: "50rem", display: "block", margin: "0 auto" }}
+        />
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
