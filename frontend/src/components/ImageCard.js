d5ccac430dfa71995d484d71b34c6f44d9dd202e
Nikola Ivkovic <nikola.ivkovic.contact@gmail.com>
2026-03-04 16:45:08 +0100

    get saved images in the frontend

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -2,17 +2,16 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const ImageCard = ({image, deleteImage}) => {
+const ImageCard = ({ image, deleteImage }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" 
-        src={image.urls.small} />
+      <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>{image.title.toUpperCase()}</Card.Title>
-        <Card.Text>
-          {image.description || image.alt_description}
-        </Card.Text>
-        <Button variant="primary" onClick={()=>deleteImage(image.id)}>Delete</Button>
+        <Card.Title>{image.title?.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
       </Card.Body>
     </Card>
   );
